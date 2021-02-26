842eb355da5c7b766b53269a037900049fb8b819
ex-nihilo88 <78040008+ex-nihilo88@users.noreply.github.com>
2021-02-26 08:05:10 -0800

    JS Clean up
    - ran a lint and cleaned up some code

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
-function menuToggle() {
+function menuToggle () {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
     x.className += ' responsive';
   } else {
     x.className = 'navtoggle';
   }
-
 }
 
 var target = $('.intro-text');
 var targetHeight = target.outerHeight();
 
-$(document).scroll(function(e) {
+$(document).scroll(function (e) {
   var scrollPercent = (targetHeight - window.scrollY) / targetHeight;
   if (scrollPercent >= 0) {
     target.css('opacity', scrollPercent);
